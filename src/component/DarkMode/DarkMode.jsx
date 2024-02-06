bb280953753fcb40f7f1ec70828e8f29c5f42f30
Abadom Emeka <abadomemekavictor@gmail.com>
2024-02-06 15:07:43 +0100

    working on responsiveness

--- src/component/DarkMode/DarkMode.jsx
+++ src/component/DarkMode/DarkMode.jsx
@@ -4,12 +4,36 @@ import { ReactComponent as Moon } from "./Moon.svg";
 import "./DarkMode.css";
 
 const DarkMode = () => {
+
+    const setDarkMode = () => {
+        document.querySelector("body").setAttribute('dark-theme', 'dark')
+        localStorage.setItem("selectedTheme", "dark");
+    }
+
+    const setLightMode = () => {
+        document.querySelector("body").setAttribute('dark-theme', 'light')
+        localStorage.setItem("selectedTheme", "light");
+    }
+
+    const selectedTheme = localStorage.getItem("selectedTheme")
+
+    if (selectedTheme === "dark") {
+        setDarkMode();
+    }
+
+    const toggleTheme = (e) => {
+        if (e.target.checked) setDarkMode();
+        else setLightMode()
+    };
+
     return (
         <div className='dark_mode'>
             <input
                 className='dark_mode_input'
                 type='checkbox'
                 id='darkmode-toggle'
+                onChange={toggleTheme}
+                defaultChecked = {selectedTheme==="dark"}
             />
             <label className='dark_mode_label' for='darkmode-toggle'>
                 <Sun />
@@ -20,3 +44,4 @@ const DarkMode = () => {
 };
 
 export default DarkMode;
+ 
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { navLinks } from '../../Data'
 import DarkMode from '../DarkMode/DarkMode'
 import { FaAlignJustify,  FaTimes} from "react-icons/fa";
 import './_navbar.scss'
+import { Variant, motion } from 'framer-motion';
 // import { useRef } from 'react';
 
 const Navbar = () => {
+
+    const [ toggle, setToggle] = useState(false);
+
+    const menuVarians = {
+        hidden: {
+            scale: 0,
+        },
+        visible: {
+            scale: 50,
+            transition: {
+                type: "tween",
+                duration: 0.5,
+            },
+
+        },
+    };
+
+    const navLinkVariants = {
+        hidden: {
+            display: "none",
+            opacity: 0
+        },
+        visible: {
+            opacity: 1,
+            y: -30,
+            transition: {
+                delay: 0.7,
+            },
+        },
+    };
+
 
 
   return (
@@ -22,10 +54,11 @@ const Navbar = () => {
                 </ul>
                 <div className='close-toggle'><FaTimes /></div>
             </div>
-            <div className="nav-toggle">
-                {/* <DarkMode /> */}
+            <div className='dark-mode'>
+                <DarkMode />
+            </div>
+            <div className = "nav-toggle">
                 <FaAlignJustify className='toggle-btn' />
-                <FaTimes />
             </div>
 
         </nav>
